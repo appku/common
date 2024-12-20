e60aaf4af5fabed5e0e6eef87353fdb6ab0093c0
Christopher Eaton <chris.eaton@appku.com>
2024-12-19 16:50:29 -0800

    Add compare to index.

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import NodeEnvironment from './node-environment.js';
 import Numbers from './numbers.js';
 import Randomization from './randomization.js';
 import Strings from './strings.js';
+import Compare from './compare.js';
 
 export {
     Cryptography,
     NodeEnvironment,
     Numbers,
     Randomization,
-    Strings
+    Strings,
+    Compare
 };
